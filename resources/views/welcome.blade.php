6e5ac1d12ea3b24f03974c6366b685c774332203
rakhaegg <rakhae.g@gmail.com>
2021-02-09 14:35:11 +0700

    update welcome blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Rakha Elang Gunawan </h1></div>
+            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Class TI-2G  </h1></div>
         </div>
     </body>
 </html>
